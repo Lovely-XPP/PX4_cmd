146fdd3ca3933ca0d918b034a3b4115e5b8711a0
Xiao_pengpeng <66028151+Lovely-XPP@users.noreply.github.com>
2022-09-23 10:39:51 +0800

    Update printf_utility & Delete unused message

--- include/utility/printf_utility.h
+++ include/utility/printf_utility.h
@@ -19,30 +19,36 @@ using namespace std;
 #define NO_POINTER "\033[?25l"
 #define POINTER "\033[?25h"
 
-/*  打印标题  */
-void print_title(std::string title_name, std::vector<string> title_content)
+/*  打印标题头  */
+void print_head(std::string title_name)
 {
     std::string title_sign = "*************************************************************";
     std::string title_space = "";
     std::string title;
-    std::string choice;
-    int choice_num = 0;
     int title_sign_size = title_sign.size();
     int title_name_size = title_name.size();
     float space_num = (title_sign_size - title_name_size) / 2.0;
-    for (int i = 0; i < floor(space_num-1); i++)
+    for (int i = 0; i < floor(space_num - 1); i++)
     {
         title_space.append(" ");
     }
     std::string tmp_space = (space_num - floor(space_num) > 0.1) ? " " : "";
-    title = title_sign + "\n" + "*" + title_space + title_name + title_space + tmp_space + 
-        "*" + "\n" + title_sign + "\n";
+    title = title_sign + "\n" + "*" + title_space + title_name + title_space + tmp_space +
+            "*" + "\n" + title_sign + "\n";
+    cout << GREEN << title << WHITE << endl;
+}
+
+/*  打印标题  */
+void print_title(std::string title_name, std::vector<string> title_content)
+{
+    print_head(title_name);
+    std::string choice;
+    int choice_num = 0;
     for (auto item = title_content.begin(); item != title_content.end(); ++item)
     {
         choice = choice + to_string(choice_num) + ". " + *item + "\n";
         choice_num++;
     }
-    cout << GREEN << title << WHITE << endl;
     cout << choice << endl;
 }
 
@@ -53,12 +59,12 @@ void Error(string msg)
 
 void Warning(string msg)
 {
-    cout << YELLOW << "[WARNING] " + msg << WHITE << endl;
+    cout << YELLOW << "[ WARN] " + msg << WHITE << endl;
 }
 
 void Info(string msg)
 {
-    cout << GREEN << "[INFO] " + msg << WHITE << endl;
+    cout << GREEN << "[ INFO] " + msg << WHITE << endl;
 }
 
 #endif
